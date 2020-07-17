2b9a8193fa3ea101ca08bf3b5e3ef707bae2c83b
deltaintegrate <deltaintegrate>
2020-07-17 13:36:02 -0500

    useState secciòn HOOK REACT

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Saludar from "./components/Saludar";
 
 function App() {
+  const [stateCar, setStateCar] = useState(false);
   const user = {
     nombre: "leo",
     edad: "99",
@@ -15,11 +16,18 @@ function App() {
 
     console.log(`hola ${name}, tiene ${edad} años.`);
   };
+
+  const encenderApagar = () => {
+    //setStateCar(!stateCar);
+    setStateCar(prevValue => !prevValue);
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Saludar userinfo={user} saludarfn={saludarfn} />
+        <h3>EL coche esta: {stateCar ? "Encendido" : "apagado"}</h3>
+        <button onClick={encenderApagar}> Encender/Apagar</button>
       </header>
     </div>
   );
